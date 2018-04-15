abff0f09eeffc97cdab56777ea35fe12c48b1d7a
Ayush Goyal <ayush987goyal@gmail.com>
2018-04-15 17:41:28 +0530

    added cache usage with fetch by cache strategy for app shell

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // SW version
-const version = '1.0';
+const version = '1.1';
 
 // Static cache - App Shell
 const appAssets = [
@@ -29,4 +29,29 @@ self.addEventListener('activate', e => {
   });
 
   e.waitUntil(cleaned);
+});
+
+// Static cache strategy - Cache with network fallback
+const staticCache = req => {
+  return caches.match(req).then(cachedRes => {
+    // Return cached response if found
+    if (cachedRes) return cachedRes;
+
+    // Fall back to network
+    return fetch(req).then(networkRes => {
+      // Update cache with new reponse
+      caches.open(`static-${version}`).then(cache => cache.put(req, networkRes));
+
+      // Return clone of response to promise chain
+      return networkRes.clone();
+    });
+  });
+};
+
+// SW Fetch
+self.addEventListener('fetch', e => {
+  //App shell
+  if (e.request.url.match(location.origin)) {
+    e.respondWith(staticCache(e.request));
+  }
 });
